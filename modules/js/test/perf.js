12ccc1ece429a3d0a2ddd56667442badad8841e4
Ningxin Hu <ningxin.hu@intel.com>
2019-05-24 13:43:23 +0800

    simplify the perf.js

--- modules/js/test/perf.js
+++ modules/js/test/perf.js
@@ -1,57 +1,134 @@
-//  //////////////////////////////////////////////////////////////////////////////////////
-//
-//  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
-//
-//  By downloading, copying, installing or using the software you agree to this license.
-//  If you do not agree to this license, do not download, install,
-//  copy or use the software.
-//
-//
-//                           License Agreement
-//                For Open Source Computer Vision Library
-//
-// Copyright (C) 2013, OpenCV Foundation, all rights reserved.
-// Third party copyrights are property of their respective owners.
-//
-// Redistribution and use in source and binary forms, with or without modification,
-// are permitted provided that the following conditions are met:
-//
-//   * Redistribution's of source code must retain the above copyright notice,
-//     this list of conditions and the following disclaimer.
-//
-//   * Redistribution's in binary form must reproduce the above copyright notice,
-//     this list of conditions and the following disclaimer in the documentation
-//     and/or other materials provided with the distribution.
-//
-//   * The name of the copyright holders may not be used to endorse or promote products
-//     derived from this software without specific prior written permission.
-//
-// This software is provided by the copyright holders and contributors "as is" and
-// any express or implied warranties, including, but not limited to, the implied
-// warranties of merchantability and fitness for a particular purpose are disclaimed.
-// In no event shall the Intel Corporation or contributors be liable for any direct,
-// indirect, incidental, special, exemplary, or consequential damages
-// (including, but not limited to, procurement of substitute goods or services;
-// loss of use, data, or profits; or business interruption) however caused
-// and on any theory of liability, whether in contract, strict liability,
-// or tort (including negligence or otherwise) arising in any way out of
-// the use of this software, even if advised of the possibility of such damage.
-//
+function standardDeviation(values){
+  var avg = average(values);
+  
+  var squareDiffs = values.map(function(value){
+    var diff = value - avg;
+    var sqrDiff = diff * diff;
+    return sqrDiff;
+  });
+  
+  var avgSquareDiff = average(squareDiffs);
 
-let testrunner = require('node-qunit');
-testrunner.options.maxBlockDuration = 20000; // cause opencv_js.js need time to load
+  var stdDev = Math.sqrt(avgSquareDiff);
+  return stdDev;
+}
 
-testrunner.run(
-    {
-        code: 'opencv.js',
-        tests: ['perf_imgproc.js'],
-    },
-    function(err, report) {
-        console.log(report.failed + ' failed, ' + report.passed + ' passed');
-        if (report.failed) {
-            process.on('exit', function() {
-                process.exit(1);
-            });
-        }
-    }
-);
+function average(data){
+  var sum = data.reduce(function(sum, value){
+    return sum + value;
+  }, 0);
+
+  var avg = sum / data.length;
+  return avg;
+}
+
+function getMs(hrtime) {
+  return hrtime[0]*1000 + hrtime[1]/1000000;
+}
+
+function printResult(elapsed, perf) {
+  console.log(`elapsed time: ${getMs(elapsed)}`);
+  console.log(`average time: ${average(perf)}`);
+  console.log(`stddev: ${standardDeviation(perf)}`);
+}
+
+const samples = 1000;
+
+var cv = require('./opencv.js');
+
+cv.onRuntimeInitialized = () => {
+  console.log('opencv.js loaded');
+  perfCvtColor();
+  perfThreshold();
+  perfGaussianBlur();
+  perfIntegral();
+};
+
+function perfCvtColor() {
+  let source = new cv.Mat(640, 480, cv.CV_8UC4);
+  let dest = new cv.Mat();
+
+  console.log(`=== cvtColor ===`);
+  let perf = [];
+  const start = process.hrtime();
+  for (let i = 0; i < samples; ++i) {
+    let hrstart = process.hrtime();
+    cv.cvtColor(source, dest, cv.COLOR_BGR2GRAY, 0);
+    let hrend = process.hrtime(hrstart);
+    perf.push(getMs(hrend));
+  }
+  const elapsed = process.hrtime(start);
+  printResult(elapsed, perf);
+
+  source.delete();
+  dest.delete();
+}
+
+function perfThreshold() {
+  const THRESHOLD = 127.0;
+  const THRESHOLD_MAX = 210.0;
+  let source = new cv.Mat(640, 480, cv.CV_8UC1);
+  let sourceView = source.data;
+  sourceView[0] = 0; // < threshold
+  sourceView[1] = 100; // < threshold
+  sourceView[2] = 200; // > threshold
+
+  let dest = new cv.Mat();
+
+  console.log(`=== threshold ===`);
+  let perf = [];
+  let start = process.hrtime();
+  for (let i = 0; i < samples; ++i) {
+    let hrstart = process.hrtime();
+    cv.threshold(source, dest, THRESHOLD, THRESHOLD_MAX, cv.THRESH_BINARY);
+    let hrend = process.hrtime(hrstart);
+    perf.push(getMs(hrend));
+  }
+  const elapsed = process.hrtime(start);
+  printResult(elapsed, perf);
+
+  source.delete();
+  dest.delete();
+}
+
+function perfGaussianBlur() {
+  let mat1 = cv.Mat.ones(640, 480, cv.CV_8UC4);
+  let mat2 = new cv.Mat();
+
+  console.log(`=== GaussianBlur ===`);
+  let perf = [];
+  let start = process.hrtime();
+  for (let i = 0; i < samples; ++i) {
+    let hrstart = process.hrtime();
+    cv.GaussianBlur(mat1, mat2, new cv.Size(3, 3), 0, 0, // eslint-disable-line new-cap
+                    cv.BORDER_CONSTANT);
+    let hrend = process.hrtime(hrstart);
+    perf.push(getMs(hrend));
+  }
+  const elapsed = process.hrtime(start);
+  printResult(elapsed, perf);
+  
+  mat1.delete();
+  mat2.delete();
+}
+
+function perfIntegral() {
+  let mat = cv.Mat.eye({height: 640, width: 480}, cv.CV_8UC1);
+  let sum = new cv.Mat();
+  let sqSum = new cv.Mat();
+  console.log(`=== integral ===`);
+  let perf = [];
+  let start = process.hrtime();
+  for (let i = 0; i < samples; ++i) {
+    let hrstart = process.hrtime();
+    cv.integral2(mat, sum, sqSum, -1, -1);
+    let hrend = process.hrtime(hrstart);
+    perf.push(getMs(hrend));
+  }
+  const elapsed = process.hrtime(start);
+  printResult(elapsed, perf);
+
+  mat.delete();
+  sum.delete();
+  sqSum.delete();
+}

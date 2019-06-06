6f5a1a1462054ad85669b7b3ef9951c18a0e87d8
wenzhao <wenzhao.xiang@intel.com>
2019-06-06 15:07:11 +0800

    set cv as a global variable

--- modules/js/test/perf_imgproc.js
+++ modules/js/test/perf_imgproc.js
@@ -1,17 +1,20 @@
-var Benchmark = require('./benchmark');
+// you may need install benchmark with npm by 
+// `npm i --save benchmark`
+
+var Benchmark = require('benchmark');
 var cv = require('./opencv.js');
 
 cv.onRuntimeInitialized = () => {
   console.log('opencv.js loaded');
-  // Benchmark.options.minSamples = 50;
+  // Benchmark.options.minSamples = 50;     // set the minimum of Samples for each case
   var suite = new Benchmark.Suite;
+  global.cv = cv;
 
   suite.add('GaussianBlur', function() {
     cv.GaussianBlur(mat1, mat2, new cv.Size(3, 3), 0, 0, // eslint-disable-line new-cap
                         cv.BORDER_DEFAULT);
   }, {
     'setup': function() {
-        var cv = require('./opencv.js');
         var mat1 = cv.Mat.ones(640, 480, cv.CV_8UC4);
         var mat2 = new cv.Mat();
         },
@@ -19,13 +22,12 @@ cv.onRuntimeInitialized = () => {
         mat1.delete();
         mat2.delete();
       }
-  })
+  });
 
   suite.add('cvtColor', function() {
     cv.cvtColor(mat1, mat2, cv.COLOR_BGR2GRAY, 0);
     }, {
       'setup': function() {
-        var cv = require('./opencv.js');
         var mat1 = cv.Mat.ones(640, 480, cv.CV_8UC3);
         var mat2 = new cv.Mat();
           },
@@ -33,13 +35,12 @@ cv.onRuntimeInitialized = () => {
         mat1.delete();
         mat2.delete();
       }
-})
+});
   
   suite.add('threshold', function() {
     cv.threshold(source, dest, THRESHOLD, THRESHOLD_MAX, cv.THRESH_BINARY);
   }, {
     'setup': function() {
-      var cv = require('./opencv.js');
       const THRESHOLD = 127.0;
       const THRESHOLD_MAX = 210.0;
       let source = new cv.Mat(640, 480, cv.CV_8UC1);
@@ -53,13 +54,12 @@ cv.onRuntimeInitialized = () => {
       source.delete();
       dest.delete();
     }
-  })
+  });
 
   suite.add('integral2', function() {
     cv.integral2(mat, sum, sqSum, -1, -1);
   }, {
     'setup': function() {
-      var cv = require('./opencv.js');
       let mat = cv.Mat.eye({height: 640, width: 480}, cv.CV_8UC1);
       let sum = new cv.Mat();
       let sqSum = new cv.Mat();
@@ -69,7 +69,7 @@ cv.onRuntimeInitialized = () => {
       sum.delete();
       sqSum.delete();
     }
-  })
+  });
 
   suite
     // add listeners
@@ -80,6 +80,7 @@ cv.onRuntimeInitialized = () => {
         console.log('stddev time:' +String(event.target.stats.deviation*1000)+' ms');
         console.log(String(event.target));
     })
+    .on('error', function(event) { console.log(event); })
     .on('complete', function() {
         console.log('Fastest is ' + this.filter('fastest').map('name'));
     })
